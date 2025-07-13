1d601c32df9a3362e4d2ba0c33a0bc2f4c51bf4d
frhandev <frhandev@gmail.com>
2025-07-13 06:17:09 +0300

    useFetch
    custom hook to fetch movies

---
+++ services/useFetch.ts
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+const useFetch = <T>(fetchFunction: () => Promise<T>, autoFetch = true) => {
+  const [data, setData] = useState<T | null>();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      //@ts-ignore
+      setError(false);
+
+      const resault = await fetchFunction();
+      setData(resault);
+    } catch (err) {
+      setError(err instanceof Error ? err : new Error("An error occured"));
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const reset = () => {
+    setLoading(false);
+    //@ts-ignore
+    setError(null);
+    setData(null);
+  };
+
+  useEffect(() => {
+    if (autoFetch) {
+      fetchData();
+    }
+  }, []);
+
+  return { data, loading, error, erfetch: fetchData, reset };
+};
+
+export default useFetch;
